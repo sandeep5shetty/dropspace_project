8130819d90228ee64deed6842359a94388bcc117
Sandeep Shetty <sandeepshettysigadalu@gmail.com>
2025-05-30 11:52:17 +0530

    one more last commit

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useEffect, useState } from "react"
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { cva, type VariantProps } from "class-variance-authority"
