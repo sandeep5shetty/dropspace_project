72854cf1f3795b994c58c8eebee439755cccd00e
Sandeep Shetty <sandeepshettysigadalu@gmail.com>
2025-06-07 02:55:21 +0530

    fixed recent files position in sm

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,10 +24,13 @@ const Dashboard = async () => {
   const usageSummary = getUsageSummary(totalSpace);
 
   return (
-    <div className="dashboard-container">      <section>        <Chart used={totalSpace.used} />
-          {/* Mobile-only file uploader */}
-        <div className="md:hidden mt-6 flex justify-center px-4">          <FileUploader 
-            ownerId={currentUser.$id} 
+    <div className="dashboard-container">
+      <section>
+        <Chart used={totalSpace.used} />
+        {/* Mobile-only file uploader */}
+        <div className="md:hidden mt-6 flex justify-center px-4">
+          <FileUploader
+            ownerId={currentUser.$id}
             accountId={currentUser.$id}
             className="w-full"
           />
@@ -66,37 +69,66 @@ const Dashboard = async () => {
           ))}
         </ul>
       </section>
-
       {/* Recent files uploaded */}
       <section className="dashboard-recent-files">
         <h2 className="h3 xl:h2 text-light-100">Recent files uploaded</h2>
         {files.documents.length > 0 ? (
           <ul className="mt-5 flex flex-col gap-5">
-            {files.documents.map((file: Models.Document) => (
-              <Link
-                href={file.url}
-                target="_blank"
-                className="flex items-center gap-3"
-                key={file.$id}
-              >
-                <Thumbnail
-                  type={file.type}
-                  extension={file.extension}
-                  url={file.url}
-                />
-
-                <div className="recent-file-details">
-                  <div className="flex flex-col gap-1">
-                    <p className="recent-file-name">{file.name}</p>
-                    <FormattedDateTime
-                      date={file.$createdAt}
-                      className="caption"
-                    />
+            {/* Show all files on md screens and up, only first 3 on smaller screens */}
+            <div className="hidden  md:block ">
+              {files.documents.map((file: Models.Document) => (
+                
+                <Link
+                  href={file.url}
+                  target="_blank"
+                  className="flex items-center gap-3 mb-4"
+                  key={file.$id}
+                >
+                  <Thumbnail
+                    type={file.type}
+                    extension={file.extension}
+                    url={file.url}
+                  />
+                  <div className="recent-file-details">
+                    <div className="flex flex-col gap-1">
+                      <p className="recent-file-name">{file.name}</p>
+                      <FormattedDateTime
+                        date={file.$createdAt}
+                        className="caption"
+                      />
+                    </div>
+                    <ActionDropdown file={file} />
                   </div>
-                  <ActionDropdown file={file} />
-                </div>
-              </Link>
-            ))}
+                </Link>
+              ))}
+            </div>
+            {/* Only first 3 files on small screens */}
+            <div className="md:hidden">
+              {files.documents.slice(0, 3).map((file: Models.Document) => (
+                <Link
+                  href={file.url}
+                  target="_blank"
+                  className="flex items-center gap-3 mb-2"
+                  key={file.$id}
+                >
+                  <Thumbnail
+                    type={file.type}
+                    extension={file.extension}
+                    url={file.url}
+                  />
+                  <div className="recent-file-details">
+                    <div className="flex flex-col gap-1">
+                      <p className="recent-file-name">{file.name}</p>
+                      <FormattedDateTime
+                        date={file.$createdAt}
+                        className="caption"
+                      />
+                    </div>
+                    <ActionDropdown file={file} />
+                  </div>
+                </Link>
+              ))}
+            </div>
           </ul>
         ) : (
           <p className="empty-list">No files uploaded</p>
