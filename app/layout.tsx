f3472de96de676a204d6310be011631972637ef9
Sandeep Shetty <sandeepshettysigadalu@gmail.com>
2025-06-07 23:42:03 +0530

    added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from 'next/font/google'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Footer from "@/components/Footer";
 
 import "./globals.css";
@@ -31,6 +32,7 @@ export default function RootLayout({
         </div>
         <Footer />
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   );
