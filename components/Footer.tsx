a8bb968727b9d7514ee57f88af151df877b86bc3
Sandeep Shetty <sandeepshettysigadalu@gmail.com>
2025-06-07 17:10:05 +0530

    fixed issue with uploader

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  return (    <footer className="fixed bottom-0 z-[99] w-full border-t bg-white py-4 text-center text-sm text-gray-500">
+  return (    <footer className="fixed bottom-0 z-[99] w-full border-t bg-white py-1 text-center text-sm text-gray-500">
       © SandeepShetty | All rights reserved
     </footer>
   );
